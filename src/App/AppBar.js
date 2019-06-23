6f94edd1a1e2fe670b06e8979dd3674155b291ff
ithingv <ithingv@naver.com>
2019-06-23 21:47:12 +0900

    OnClick Button

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -29,8 +29,10 @@ function toProperCase(lower){
 function ControlButton({name, active}){
     return (
         <AppContext.Consumer>
-            {({page}) => (
-        <ControlButtonElem active={page === name}>
+            {({page, setPage}) => (
+        <ControlButtonElem 
+            active={page === name}
+            onClick={()=> setPage(name)}>
             {toProperCase(name)}
         </ControlButtonElem>
             )}
