1d4b24fc3800cd6dd77821dbfc85b95f3ed956dd
ithingv <ithingv@naver.com>
2019-06-24 21:06:51 +0900

    Add / Remove Coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -2,31 +2,67 @@ import React from 'react';
 
 const cc = require('cryptocompare');
 export const AppContext = React.createContext();
+const MAX_FAVORITES = 10;
 
 export class AppProvider extends React.Component{
     constructor(props){
         super(props);
             this.state = {
-                page: 'dashboard',
+                page: 'dashboard',      
+                favorites: ['BTC', 'LTC', 'DOGE', 'ETH'],
                 ...this.saveSettings(),
                 setPage: this.setPage,
+                addCoin: this.addCoin,
+                removeCoin: this.removeCoin,
+                isInFavorites: this.isInFavorites,
+                isFavoriteListMaxed: this.isFavoriteListMaxed,
                 confirmFavorites : this.confirmFavorites
             }
     } 
+
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if(favorites.length < MAX_FAVORITES){
+            favorites.push(key);
+            this.setState({favorites});
+        }
+    }
+
+
+
     componentDidMount = () =>{
         this.fetchCoins();
     }
     fetchCoins = async() => {
         let coinList = (await cc.coinList()).Data;
+        console.log(coinList);
         this.setState({coinList});
     }
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if(!this.isFavoriteListMaxed() && !this.isInFavorites(key)) {
+          favorites.push(key);
+          this.setState({favorites});
+        }
+      }
+    
+      removeCoin = key => {
+        this.setState({
+          favorites: this.state.favorites.filter(favorite => favorite !== key)
+        });
+      }
+    
+      isInFavorites = key => ~this.state.favorites.indexOf(key);
+    
+      isFavoriteListMaxed = () => this.state.favorites.length >= MAX_FAVORITES;
     confirmFavorites = () => {
         this.setState({
             firstVisit: false,
             page: 'dashboard'
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
-            test: 'hello'
+            
+          favorites: this.state.favorites,
         }));
     }
 
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
-import {AppContext} from "../App/AppProvider";
-import {SelectableTile} from "../Shared/Tile";
-import CoinTile from "./CoinTile"
-
+import styled from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+import CoinTile from './CoinTile';
 export const CoinGridStyled = styled.div`
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-gap: 15px;
-    margin-top:40px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
+  margin-top: 30px;
+`;
 
-`
-function getCoinsToDisplay(coinList, topSection){
-    return Object.keys(coinList).slice(0, topSection ? 10 : 100);
-}
+const getCoinsToDisplay = (coinList, topSection, favorites) => {
+  return topSection ? favorites : Object.keys(coinList).slice(0, 100);
+};
 
-
-export default function({topSection}){
-    return <AppContext.Consumer>
-        {({coinList}) => <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection).map(coinKey =>
-                <CoinTile topSection={topSection} coinKey = {coinKey}/>
-            )}
-        </CoinGridStyled>}
-    </AppContext.Consumer>    
+export default function({topSection}) {
+  return (
+    <AppContext.Consumer>
+      {({coinList, favorites}) => (
+        <CoinGridStyled>
+          {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => {
+             return (
+              <CoinTile
+                topSection={topSection}
+                coinKey={coinKey}
+                key={coinKey}
+              />
+            );
+          })}
+        </CoinGridStyled>
+      )}
+    </AppContext.Consumer>
+  );
 }
